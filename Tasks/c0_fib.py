1e0b4b590b9bb67c042348b83bf5ae7ac761ae7c
Тихонова Ирина Геннадьевна <sondrafinchley@mail.ru>
2022-09-23 17:22:40 +0300

    fib, fact, recursive binary

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -1,3 +1,6 @@
+import time
+
+
 def fib_recursive(n: int) -> int:
     """
     Calculate n-th number of Fibonacci sequence using recursive algorithm
@@ -30,7 +33,36 @@ def fib_iterative(n: int) -> int:
         num_1 = 1
         num_2 = 1
         for _ in range(3, n+1):
-            mid_result = num_1 + num_2
-            num_1, num_2 = num_2, mid_result
+            num_1, num_2 = num_2, num_1 + num_2
 
-        return mid_result
+        return num_1
+
+
+def fib_gen(n: int):
+    if n < 0:
+        raise ValueError(f"Is there a {n} number of Fibonacci sequence? I think here should be and exception...")
+
+    num_1, num_2 = 0, 1
+    for _ in range(0, n+1):
+        yield num_1
+        num_1, num_2 = num_2, num_1 + num_2
+
+
+if __name__ == '__main__':
+    t_1 = time.perf_counter_ns()
+
+    for i in range(10):
+        print(fib_recursive(33))  # 33 without lru 0.81994952 | 33 with lru 4.03e-06
+        # print(fib_iterative(301))  # Очень быстро 10^-5
+
+    print(((time.perf_counter_ns() - t_1) / 10) / 1000 / 1000 / 1000)
+
+    f_gen = fib_gen(9)
+    print(next(f_gen))
+    print(next(f_gen))
+    print(next(f_gen))
+    print(next(f_gen))
+    print(next(f_gen))
+    print(next(f_gen))
+    print(next(f_gen))
+    print(next(f_gen))
