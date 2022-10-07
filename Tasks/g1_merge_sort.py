4ea1979762af058d8348a69240aec02f11e3abd3
Тихонова Ирина Геннадьевна <sondrafinchley@mail.ru>
2022-10-07 17:14:54 +0300

    sort commit

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,16 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    if len(container) == 1:
+        return container
+
+    final_result = []
+    middle = len(container)//2
+
+    while container[0:middle] or container[middle:-1]:
+        sort(container[0:middle])
+
+
+
+
     return container
--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -8,4 +8,14 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    if len(container) <= 1:
+        return container
+
+    pivot_index = len(container) // 2
+    pivot_value = container[pivot_index]
+
+    return (
+            sort([v for v in container if v < pivot_value]) +
+            [v for v in container if v == pivot_value] +
+            sort([v for v in container if v > pivot_value])
+    )
